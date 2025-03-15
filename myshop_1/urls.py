d9b699393c621ea2781a50a4380b9040dbd76c02
ForexOL <zantamore@gmail.com>
2025-03-16 00:36:51 +0300

    6

--- myshop_1/urls.py
+++ myshop_1/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from . import settings
 
-from django.conf.urls import url
 from django.views.static import serve
 from django.conf.urls import (
                                 handler400,
