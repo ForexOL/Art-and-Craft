02c728ac69beb1bb5f8443d926204bc0cd69c8a8
ForexOL <zantamore@gmail.com>
2025-03-16 00:17:52 +0300

    Update settings.py

--- myshop_1/settings.py
+++ myshop_1/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
 '''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mydatabase_arcrid',
         'USER': 'myuser_arcrid',
         'PASSWORD': '1234$VictoriaLurvin',
