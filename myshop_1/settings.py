50f361980cd7df85b601c1eb361989a1a858c857
ForexOL <zantamore@gmail.com>
2025-03-29 07:14:25 +0300

    git push

--- myshop_1/settings.py
+++ myshop_1/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'myshop_1.wsgi.application'
 
 
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -130,7 +130,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-
+"""
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
