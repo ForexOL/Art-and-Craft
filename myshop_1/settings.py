510a2f960c6555322a6fab30877b7d31a66ebe8d
ForexOL <zantamore@gmail.com>
2025-04-09 11:04:57 +0300

    update migrations

--- myshop_1/settings.py
+++ myshop_1/settings.py
@@ -111,8 +111,8 @@ WSGI_APPLICATION = 'myshop_1.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
